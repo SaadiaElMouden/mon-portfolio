b9b80ae615ef14eb09fea803150618c28c1bdf38
Saadia El Mouden <saadiae474@gmail.com>
2026-03-23 00:50:45 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold">Tailwind fonctionne</h1>
+      <p className="mt-4 text-lg">Mon portfolio React est bien configuré.</p>
+    </main>
+  );
+}
+
+export default App;
